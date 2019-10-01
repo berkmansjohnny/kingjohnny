fb27da29f9fcff76169e28759740f2e1abc7fd7a
KingJohnny <johnny@thinktomorrow.be>
2019-10-01 08:01:27 +0200

    new landingpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,12 @@
 ;(function () {
-
-	'use strict';
-
-	// iPad and iPod detection
-	var isiPad = function(){
-		return (navigator.platform.indexOf("iPad") != -1);
-	};
-
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) ||
-			(navigator.platform.indexOf("iPod") != -1)
-	    );
-	};
-
-	// Parallax
-	var parallax = function() {
-		$(window).stellar();
-	};
-
-	// Dribbble
-	$.jribbble.setToken('4655f9e2640560e7e2fc7b6672d918eb4119bb496d6cf75dfce01ed178fe7738');
-	$.jribbble.users('kingjohnny').shots({
-				'per_page': 3,
-				'sort': 'views'
-			}).then(function(res) {
-				var html = [];
-				res.forEach(function(shot) {
-					html.push('<div class="card inset-s column-4 shots--shot">');
-					html.push('<div class="king-blog">');
-					html.push('<a href="' + shot.html_url + '" target="_blank">');
-					html.push('<img class="img-responsive" src="' + shot.images.normal + '">');
-					html.push('</a></div></div>');
-				});
-
-				$('.shots').html(html.join(''));
-			});
-
-	// Animations
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							el.addClass('fadeInUp animated');
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-
-				}, 100);
-
-			}
-
-		} , { offset: '85%' } );
-	};
-	$(function(){
-			var greeting;
-			var time = new Date().getHours();
-				if (time < 10) {
-						greeting = "Good morning";
-				} else if (time < 20) {
-						greeting = "Good day";
-				} else {
-						greeting = "Good evening";
-				};
-				$('title').append(greeting);
-		});
-	// Document on load.
-	$(function(){
-		parallax();
-		contentWayPoint();
-	});
-}());
+	var greeting;
+	var time = new Date().getHours();
+		if (time < 11) {
+				greeting = "Good morning";
+		} else if (time < 18) {
+				greeting = "Good day";
+		} else {
+				greeting = "Good evening";
+		};
+		$('title').append(greeting);
+});
--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="shortcut icon" href="favicon.ico">
 		<meta name="theme-color" content="#291a2d">
-		<link rel="stylesheet" href="assets/css/king.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.1.2/tailwind.css" />
 		<link rel="apple-touch-icon" sizes="192x192" href="assets/favicons/apple-icon-precomposed.png">
-		<link rel="icon" type="image/png" sizes="192x192"  href="assets/favicons/favicon.png">
 		<link rel="icon" type="image/png" sizes="192x192" href="assets/favicons/favicon.png">
+		<link rel="stylesheet" href="assets/css/main.css" />
 
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800|Slabo+27px" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900" rel="stylesheet">
@@ -27,40 +27,21 @@
 
 	</head>
 	<body>
-	<div id="king-wrapper">
-		<div id="king-page">
-			<?php include('views/blocks/nav.php') ?>
-			<?php include('views/blocks/hero.php') ?>
-			<?php include('views/blocks/tools.php') ?>
-			<?php include('views/blocks/skills.php') ?>
+	<main class="text-center md:text-left md:flex flex-row h-screen items-center">
+		<section class="md:w-1/2 md:pl-48 content-between">
+			<img src="assets/svg/logo.svg" title="Kingjohnny" class="hidden md:block mx-auto w-20 md:absolute top-0 pt-4" />
 
-			<?php include('views/vendor/dribbble.php') ?>
-
-			<?php include('views/blocks/footer.php') ?>
-
-	</div>
-
-	</div>
-
-	<!-- jQuery -->
-	<script src="assets/js/vendor/jquery.min.js"></script>
-	<script src="assets/js/vendor/jribble.js"></script>
-
-	<script>
-	$(function(){
-
-	});
-	</script>
-
-	<!-- jQuery Easing -->
-	<script src="assets/js/vendor/jquery.easing.1.3.js"></script>
-
-	<!-- Waypoints -->
-	<script src="assets/js/vendor/jquery.waypoints.min.js"></script>
-	<!-- Stellar -->
-	<script src="assets/js/vendor/jquery.stellar.min.js"></script>
-	<!-- Main JS -->
+			<img src="assets/images/berkmansjohnny.png" alt="berkmansjohnny" class="mx-auto w-48 mt-4 md:hidden">
+			<h2 class="font-bold text-4xl text-yellow-600 py-12 md:py-4">Ik ben Berkmans Johnny. Een enthousiaste designer & front-end developer. Uit de Kempen.</h2>
+			<h3 class="text-gray-500">Webdesigner @ <a href="https://thinktomorrow.be" target="_blank" class="underline text-gray-600">Think Tomorrow</a> | Ballroom Dancer | Pixellover </h3></div>
+			<div class="md:absolute bottom-0 text-gray-500 pb-4">&copy; 2019. <a href="https://dribbble.com/kingjohnny" target="_blank" class="underline text-gray-600">Dribble</a>. <a href="https://twitter.com/kingjohnny" target="_blank" class="underline text-gray-600">Twitter</a>. <a href="https://www.instagram.com/berkmansjohnny/" target="_blank" class="underline text-gray-600">Instagram</a>.</div>
+		</section>
+		<section class="hidden md:block md:w-1/2 h-full relative">
+			<img src="assets/images/berkmansjohnny.jpg" class="md:absolute bottom-0 left-0 mx-auto my-auto right-0 rounded shadow-2xl top-0 w-2/3 z-10">
+			<div class="bg-animate w-full md:w-1/2 h-48 md:h-screen bg-cover float-right" style="background-image:url('assets/images/hero/forest.jpg');"></div>
+		</section>
+		
+	</main>
 	<script src="assets/js/main.js"></script>
-
 	</body>
 </html>
